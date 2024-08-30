4d27c3ab7e5ce04c4b8e9923e4838ac4408d918c
creditvilleNg <chioma.chukelu@creditville.ng>
2024-08-30 18:14:53 +0100

    update

---
+++ src/main/java/interswitch/assessment/discoveryfintechdigitalonly/DiscoveryfintechdigitalonlyApplication.java
@@ -0,0 +1,15 @@
+package interswitch.assessment.discoveryfintechdigitalonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryfintechdigitalonlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryfintechdigitalonlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/interswitch/assessment/discoveryfintechdigitalonly/DiscoveryfintechdigitalonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package interswitch.assessment.discoveryfintechdigitalonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryfintechdigitalonlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
