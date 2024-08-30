9ad1e1aa720f04f26bdfd7d8146cf0eb6edb218d
creditvilleNg <chioma.chukelu@creditville.ng>
2024-08-30 20:04:13 +0100

    update

--- src/main/java/interswitch/assessment/discoveryfintechdigitalonly/DiscoveryfintechdigitalonlyApplication.java
+++ src/main/java/interswitch/assessment/discoveryfintechdigitalonly/DiscoveryfintechdigitalonlyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//THis annotation is need to indicate that this is a discovery server and this will be available after the library has been imported
 @EnableEurekaServer
 public class DiscoveryfintechdigitalonlyApplication {
 
